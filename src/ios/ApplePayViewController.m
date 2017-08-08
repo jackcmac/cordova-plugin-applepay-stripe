0cca8601ef4988b49de84994056ddf2c3a461b55
jackcmac <30657263+jackcmac@users.noreply.github.com>
2017-08-08 08:22:39 -0400

    Update ApplePayViewController.m

--- src/ios/ApplePayViewController.m
+++ src/ios/ApplePayViewController.m
@@ -1,1 +1,62 @@
 
+#import "ApplePayViewController.h"
+#if CORDOVA_VERSION_MIN_REQUIRED < __CORDOVA_4_0_0
+#import <Cordova/CDVJSON.h>
+#endif
+
+
+@implementation ApplePayViewController
+NSDictionary *initOptions;
+
+- (id)initWithOptions:(NSDictionary *) options {
+    self = [super init];
+    initOptions = [[NSDictionary alloc] initWithDictionary:options];
+    self.plugins = [NSMutableDictionary dictionary];
+    self.isFullScreen = NO;
+    self.embedRect = nil;
+    self.screenSize = [[UIScreen mainScreen] bounds];
+
+    return self;
+}
+
+- (void)loadView {
+  [super loadView];
+  [self updatePayViewLayout];
+}
+- (void)updatePayViewLayout {
+  
+  if (self.isFullScreen == NO) {
+    [self.view setFrameWithDictionary:self.embedRect];
+  }
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.view.backgroundColor = [[NSArray arrayWithObjects:[NSNumber numberWithInt:128],
+                                                            [NSNumber numberWithInt:128],
+                                                            [NSNumber numberWithInt:128],
+                                                            [NSNumber numberWithInt:128], nil] parsePluginColor];
+  
+    //------------
+    // Initialize
+    //------------
+    self.overlayManager = [NSMutableDictionary dictionary];
+  
+    CGRect pluginRect = self.view.frame;
+    int marginBottom = 0;
+    CGRect payRect = CGRectMake(0, 0, pluginRect.size.width, pluginRect.size.height  - marginBottom);
+    //NSLog(@"mapRect=%f,%f - %f,%f", mapRect.origin.x, mapRect.origin.y, mapRect.size.width, mapRect.size.height);
+    //NSLog(@"mapRect=%@", camera);
+    self.pay = [ApplePayView mapWithFrame:payRect camera:camera];
+    self.pay.delegate = self;
+    //self.map.autoresizingMask = UIViewAutoresizingNone;
+    self.map.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+  
+    //indoor display
+    self.pay.indoorDisplay.delegate = self;
+  
+  
+    
+    [self.view addSubview: self.pay];
+}
